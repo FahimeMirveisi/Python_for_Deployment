42ab2a07cfcb222a006797ef5c09bd15e6da11b8
FahimeMirveisi <fahime.mir75@gmail.com>
2024-03-29 20:48:33 +0330

    class code for assignment3 completed

--- Christopher_Nolan's_movies_API_assignment3/classcode3/main.py
+++ Christopher_Nolan's_movies_API_assignment3/classcode3/main.py
@@ -17,11 +17,11 @@ def test():
 
 @app.get("/salam/{firstname}")
 def test2(firstname: str, lastname:str = "Hosseini"):
-    return "علیک سلام" + firstname + " " + lastname + "جان"
+    return "علیک سلام" + " " + firstname + " " + lastname+ " " + "جان"
 
 @app.get("/salam/{firstname}/{lastname}")
 def test3(firstname: str, lastname:str = "Hosseini"):
-    return "علیک سلام" + firstname + " " + lastname + "جان"
+    return "علیک سلام" +" " + firstname + " " + lastname + " " + "جان"
 
 @app.get("/tv-channel/{name}")
 def test4(name: Union[str, int]):
@@ -34,7 +34,7 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.get("/create-image/{red}/{green}/{blue}")
 def create_image(red: int, green: int, blue:int):
     if 0<= red <= 255 and 0<= green <= 255 and 0<= blue <= 255:
-        image = np.zeros((300, 200 3), dtype= np.uint8)
+        image = np.zeros((300, 200 ,3), dtype= np.uint8)
         image[:, :] = (red, green, blue)
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         # cv2.imwrite("text.jpg", image)
