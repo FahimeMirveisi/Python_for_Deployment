aa371d7592a8cf85315c4bc2f272f4e2b5d9ff97
FahimeMirveisi <fahime.mir75@gmail.com>
2024-12-25 14:48:37 +0330

    start django

---
+++ API_FastAPI/Christopher_Nolan_API_assignment3/classcode3/main2.py
@@ -0,0 +1,50 @@
+from typing import Union
+import numpy as np
+import cv2
+from fastapi import FastAPI, HTTPException, status
+from fastapi.responses import StreamingResponse
+import io
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "world"}
+
+@app.get("/salam")
+def test():
+    return "علیک سلام"
+
+@app.get("/salam/{firstname}")
+def test2(firstname: str, lastname: str = "hosseini"):
+    return " علیک سلام " + firstname + " " + lastname + " جان " 
+
+@app.get("/salam/{firstname}/{lastname}")
+def test3(firstname: str, lastname: str = "hosseini"):
+    return " علیک سلام " + firstname + " " + lastname + " جان " 
+
+@app.get("/tv-channel/{name}")
+def test4(name: Union[int, str]):
+    return {"name" : name}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q:Union[str, None] = None):
+    return{"item_id":item_id, "q":q}
+
+
+@app.get("/create-image/{red}/{green}/{blue}")
+def image(red: int, green: int, blue: int):
+    if 0<=red<=255 and 0<=green<=255 and 0<=blue<=255:
+        image = np.zeros((300, 200, 3), dtype=np.uint8)
+        image[:, :] = (red, green, blue)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        #cv2.imwrite("testim.png", image)
+        _, encoded_image = cv2.imencode(".png", image)
+
+        return StreamingResponse(io.BytesIO(encoded_image.tobytes()), media_type="image/png")
+    
+    else:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
+                             detail="Numbers must be between 0 and 255")
